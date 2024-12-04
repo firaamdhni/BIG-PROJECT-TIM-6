721bb27afc4692c9e07a19ff57709c528220d12f
Ulfakhairinaa <khairinaulfa328@gmail.com>
2024-12-04 16:02:46 +0700

    Update function.c

--- Tugas 1/function.c
+++ Tugas 1/function.c
@@ -28,3 +28,33 @@ void data() {
     }
     fclose(file);
 }
+
+// Fungsi untuk menyimpan data ke file
+void simpan_data() {
+    FILE *file = fopen("alat_lab.txt", "w");
+    for (unsigned int i = 0; i < total_alat; i++) {
+        fprintf(file, "%u %s %s %s %u %u %u\n",
+                alat_lab[i].Id_Alat,
+                alat_lab[i].Nama_Alat,
+                alat_lab[i].Merek,
+                alat_lab[i].Model,
+                alat_lab[i].Tahun_Produksi,
+                alat_lab[i].Jumlah_Unit,
+                alat_lab[i].Jumlah_Tersedia);
+    }
+    fclose(file);
+}
+
+// Menu Admin
+void admin_menu() {
+    int pilih;
+    do {
+        printf("\n---- Menu Admin ----\n");
+        printf("1. Tambah Alat Lab\n");
+        printf("2. Lihat Daftar Alat Lab\n");
+        printf("3. Edit Alat Lab\n");
+        printf("4. Hapus Alat Lab\n");
+        printf("5. Keluar\n");
+        printf("Pilih Opsi: ");
+        scanf("%d", &pilih);
+
